5b892437509e6a7eb0484995671e91b0d9c03742
이소윤 <s_lee77176@fanshaweonline.ca>
2020-11-22 17:17:21 +0900

    date

--- src/App.js
+++ src/App.js
@@ -16,21 +16,29 @@ function FuncComp(props){
   const numberState = useState(props.initNumber);
   let number = numberState[0];
   let setNumber = numberState[1];
+
+  const dateState = useState((new Date()).toString());
+  let _date = dateState[0];
+  let setDate = dateState[1];
   return(
 <div className="container">
   <h2>Functional Style Component</h2>
   <p>Number: {number}</p>
   <input type="button" value="random"
          onClick={function(){
-           setNumber(Math.random()*100)
-           }}></input>
+           setNumber(Math.random()*100)}}></input>
+<p>Date : {_date} </p>
+<input type="button" value="new date" onClick={function(){
+setDate(new Date().toString())
+}}></input>
 </div>    
   );
 }//do setNumber instead of setState
 
 class ClassComp extends Component{
   state = {
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date:(new Date()).toString()
   }
   render(){
     return(
@@ -40,7 +48,12 @@ class ClassComp extends Component{
         <input type="button" value="random"
          onClick={function(){
            this.setState({number:(Math.random()*100)})}.bind(this)}></input>
-      </div>
+        <p>Date : {this.state.date}</p>
+      <input type="button" value="New Date"
+      onClick={function(){
+        this.setState({date:(new Date()).toString()})}
+        .bind(this)}></input>
+   </div>
     )
 
   };
